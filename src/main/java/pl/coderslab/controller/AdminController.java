4c89dc166841c2c1bf0a2e8b7030c524c54818b4
EwaBujara <ewa.bujara@gmail.com>
2019-01-30 13:17:48 +0100

    lejzi czrabol

--- src/main/java/pl/coderslab/controller/AdminController.java
+++ src/main/java/pl/coderslab/controller/AdminController.java
@@ -7,11 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.entity.Group;
 import pl.coderslab.entity.Role;
 import pl.coderslab.entity.User;
 import pl.coderslab.repository.RoleRepository;
 import pl.coderslab.repository.UserRepository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Controller
@@ -24,13 +26,15 @@ public class AdminController {
     @Autowired
     RoleRepository roleRepository;
     @GetMapping("/edit/{id}")
+    @Transactional
     public String add(
             Model model,
             @PathVariable Long id){
 
         User user = userRepository.findOne(id);
         model.addAttribute("user", user);
-        model.addAttribute("groups",user.getGroups());
+        List<Group> groups = user.getGroups();
+        model.addAttribute("groups",groups);
         return "admin/completeUserAccount";
     }
 
